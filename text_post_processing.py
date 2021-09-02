db34adc34daca1f52f7118cff99c8438979830b0
nidolight <70171637+nidolight@users.noreply.github.com>
2021-09-02 20:39:57 +0900

    Add files via upload

--- text_post_processing.py
+++ text_post_processing.py
@@ -3,34 +3,28 @@
 from jamo import h2j, j2hcj
 from hangul_utils import join_jamos
 from difflib import SequenceMatcher
+import pickle
 
 def find_word(text):
     word = j2hcj(h2j(text))
-    # word_dic = []  # dic 불러오기
-    # f = open("word_dic.txt", 'r', encoding='utf-8')
-    # lines = f.readlines()
-    # for line in lines:
-    #     word_dic.append(line)
-    # f.close()
-    word_dic = [["ㅁㅐㅇㅣㄹㅇㅜㅇㅠ"], [
-        "ㄷㅗㄴㅇㅠㄱㄷㅡㅇㅅㅣㅁㄷㅓㅅㅅㅏㄹ"], ["ㅅㅡㅍㅡㄹㅏㅇㅣㅌㅡ"], ["ㄷㅗㄴㅇㅠㄱㅎㅏㅇㅈㅓㅇㅅㅏㄹ"], ["ㄷㅗㄴㅇㅠㄱㅁㅗㄱㅅㅣㅁ"]
-        ["ㄲㅐㅅㅇㅣㅍ"], ["ㅍㅗㅋㅏㅊㅣㅂ"]]
+    with open('dict.pkl', 'rb') as f:
+        mydict = pickle.load(f)
 
     tmp = 0
     ans = "null"
-    for dic in word_dic:
-        similarity = similar(word, str(dic))
+    for key, val in mydict.items():
+        similarity = similar(word, key)
         if similarity > 0.35 and similarity > tmp:
             tmp = similarity
-            ans = dic
+            ans = key
+    print("similairity : " + str(tmp))
+    print("ans : " + str(ans))
     ans = join_jamos(str(ans))
         
     return ans
 
-
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-
 if __name__ == "__main__":
-    print(find_word("포카칩"))
+    print(find_word("동육앙정살"))
